b9b796e793ccd9737847927a2a7c4e647d20eba0
Devejya Pandey <devejya.23fe10cse00662@muj.manipal.edu>
2026-03-26 22:48:04 +0530

    Update base path for GitHub Actions deployment

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/portfolio-website/',
+  base: process.env.GITHUB_ACTIONS ? '/portfolio-website/' : '/',
 })
